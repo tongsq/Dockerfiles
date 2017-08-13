834f7b7206e86bd943420126728410328ec29284
tongsq <tongsq77@qq.com>
2017-08-13 20:24:12 +0800

    init apache-php

---
+++ apache-php/download.sh
@@ -0,0 +1,4 @@
+wget -O ./httpd-2.4.27.tar.gz http://mirrors.tuna.tsinghua.edu.cn/apache//httpd/httpd-2.4.27.tar.gz
+wget -O ./php-7.1.8.tar.gz http://au1.php.net/get/php-7.1.8.tar.gz/from/this/mirror
+wget -O ./apr-1.6.2.tar.gz http://apache.fayea.com//apr/apr-1.6.2.tar.gz
+wget -O ./apr-util-1.6.0.tar.gz http://mirrors.tuna.tsinghua.edu.cn/apache//apr/apr-util-1.6.0.tar.gz
